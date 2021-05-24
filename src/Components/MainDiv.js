5af20f4305ef0258382123de2cb44fb4e170774c
mandykparson <79546411+mandykparson@users.noreply.github.com>
2021-05-24 13:19:43 -0600

    setting state of MainDiv

--- src/Components/MainDiv.js
+++ src/Components/MainDiv.js
@@ -2,13 +2,40 @@ import React from 'react'
 import UserInput from "./UserInput"
 import ClimberContainer from "./ClimberContainer"
 
-export default function MainDiv() {
-    
-    return (
-        <div>
-            <p>Main Content</p>
-            <UserInput />
-            <ClimberContainer />
-        </div>
-    )
+export default class MainDiv extends React.Component {
+
+    state = {
+        climbers: [],
+        areas: []
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3000/climbers')
+            .then(res => res.json())
+            .then(result => {
+                this.setState({ 
+                    climbers: result })
+            })
+    }
+    componentDidMount() {
+        fetch('http://localhost:3000/areas')
+            .then(res => res.json())
+            .then(result => {
+                this.setState({ 
+                    areas: result })
+            })
+    }
+
+
+    render() {
+        console.log(this.state.climbers)
+        console.log(this.state.areas)
+        return (
+            <div>
+                <p>Main Content</p>
+                <UserInput />
+                <ClimberContainer climbers={this.state.climbers}/>
+            </div>
+        )
+    }
 }
